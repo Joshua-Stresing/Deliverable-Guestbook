ec0932f32e97fc09f750768abca2a9c571eb4e5e
Joshua Stresing <pyrotorch69@yahoo.com>
2022-05-05 17:15:11 -0700

    got auth page loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import Auth from './views/Auth/Auth';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+// import { useState } from 'react';
+// import { getUser } from './services/user';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/">
+            <Auth />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ src/components/guestbook.js
@@ -0,0 +1,5 @@
+// import React, { useEffect, useState } from 'react';
+
+// export default function guestBook() {
+//   const [error, setError] = useState('');
+// }
