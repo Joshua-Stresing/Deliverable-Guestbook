3f1b256a93382010156e267a21ec74b6a9b74320
Joshua Stresing <pyrotorch69@yahoo.com>
2022-05-13 17:47:54 -0700

    passing with a garbage test

--- App.test.jsx
+++ App.test.jsx
@@ -1,8 +1,16 @@
 import { screen, render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom/cjs/react-router-dom.min';
 import App from './src/App';
+import { UserProvider } from './src/context/context';
 
 describe('main', () => {
-  it.skip('main has', async () => {
-    render(<App />);
+  it('main has', async () => {
+    render(
+      <UserProvider>
+        <MemoryRouter>
+          <App />;
+        </MemoryRouter>
+      </UserProvider>
+    );
   });
 });
